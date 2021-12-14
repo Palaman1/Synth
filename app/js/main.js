3eea4a5e75339e5ca016cd2293a482cd4567d2d1
Palaman1 <palaman12345@gmail.com>
2021-12-14 13:37:05 +0200

    added new files

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,15 @@ $(function () {
     $('.header-tabs__top-item').removeClass('header-tabs__top-item--active');
     $(this).addClass('header-tabs__top-item--active');
 
+   
+    $('.header-tabs__content-item').removeClass('header-tabs__content-item--active');
     $($(this).attr('href')).addClass('header-tabs__content-item--active');
   });
 
-  $('.posts__cards-share').one('click', function (){
+  $('.posts__cards-share').one('click', function () {
     $(this).addClass('posts__cards-share--disable');
-    
-    $('.posts__cards-item').click(function(){
+
+    $('.posts__cards-item').click(function () {
       $(this).children('p').css('opacity', '1');
     });
   });
